9742317e69b90cd1fac0555441495b8943688017
Muba456611 <zulumubanga7@gmail.com>
2025-04-19 23:11:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,38 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
+function convertToBool(text, fault = 'true') {name: Node.js CI
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
     return text === fault ? true : false;
 }
 
